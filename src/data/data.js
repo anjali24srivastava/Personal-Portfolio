dcd3c70f72ceaaa46c1c55ed726638bb7246ea12
anjali24srivastava <anjalisrivastava118@gmail.com>
2024-09-05 15:45:48 +0530

    final-commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,4 @@
 import { AiFillAppstore } from "react-icons/ai";
-import {  FaBrain} from "react-icons/fa";
-import { SiProgress} from "react-icons/si";
 import { CgWebsite } from "react-icons/cg";
 import { MdAnalytics } from "react-icons/md";
 
